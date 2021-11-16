25d96b8a9fcfd3257fc1dbb0c41b839ae8b84812
leon <leon@leo.leo.mil>
2021-11-16 06:50:47 -0500

    BOF python code

---
+++ createbadchar.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/env python3
+
+from __future__ import print_function
+
+#tart with 00 and add any others you find
+bad = "00 07 2e a0".split()
+
+#turns them into a nice string to copy into python
+print("badchars = ")
+for x in range(1, 256):
+  if "{:02x}".format(x) not in bad: 
+    print("\\x" + "{:02x}".format(x), end='')
+
+#creates a nice string to use in Mona
+print("\n\nfor mona")
+for byte in bad:
+  print("\\x{}".format(byte), end='')
+print()
